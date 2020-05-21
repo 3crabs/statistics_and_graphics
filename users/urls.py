a19ecc51a4baf9433e6bb2fa8b8ef5c5cda1bda8
FedorovVladimir <vladimir.fodorow@gmail.com>
2020-05-22 01:27:33 +0700

    add page user info

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from users import views
 
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^$', views.get_users),
+    url(r'^(?P<user_id>\d+)$', views.get_one_user),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 
-def index(request):
+def get_users(request):
     return render(request, 'users.html', locals())
+
+
+def get_one_user(request, user_id):
+    return render(request, 'user_info.html', locals())
