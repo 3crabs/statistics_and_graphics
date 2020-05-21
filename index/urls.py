6229488653ef5ab16a8be3b6e9c71ae729ae16ae
FedorovVladimir <vladimir.fodorow@gmail.com>
2020-05-22 01:32:04 +0700

    add page course info

--- index/urls.py
+++ index/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from index import views
 
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^$', views.get_courses),
+    url(r'^(?P<course_id>\d+)$', views.get_one_course),
 ]
--- index/views.py
+++ index/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 
-def index(request):
+def get_courses(request):
     return render(request, 'index.html', locals())
+
+
+def get_one_course(request, course_id):
+    return render(request, 'course_info.html', locals())
