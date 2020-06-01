a03c2fb97814826baa8cb9222d269bdfc7c2425f
FedorovVladimir <vladimir.fodorow@gmail.com>
2020-06-01 13:32:22 +0700

    change group cohort

--- groups/views.py
+++ groups/views.py
@@ -5,30 +5,30 @@ from users.models import User
 
 
 def get_groups(request):
-    sql = "select mdl_groups.id as id, " \
-          "       mdl_groups.name as name, " \
+    sql = "select cohort.id as id, " \
+          "       cohort.name as name, " \
           "(select count(*) " \
           "        from mdl_user as user, " \
-          "             mdl_groups_members as groups_members " \
-          "        where groups_members.userid = user.id " \
-          "          and groups_members.groupid = mdl_groups.id) as count_users " \
-          "from mdl_groups"
+          "             mdl_cohort_members as cohort_members " \
+          "        where cohort_members.userid = user.id " \
+          "          and cohort_members.cohortid = cohort.id) as count_users " \
+          "from mdl_cohort as cohort"
     groups = Group.objects.raw(sql)
     return render(request, 'final_grade.html', locals())
 
 
 def get_one_group(request, group_id):
-    sql = "select mdl_groups.id as id, " \
-          "       mdl_groups.name as name " \
-          "from mdl_groups " \
-          "where mdl_groups.id = " + str(group_id)
+    sql = "select cohort.id as id, " \
+          "       cohort.name as name " \
+          "from mdl_cohort as cohort " \
+          "where cohort.id = " + str(group_id)
     group = Group.objects.raw(sql)[0]
 
     sql = "select user.id as id, " \
           "       user.firstname as name " \
           "from mdl_user as user, " \
-          "     mdl_groups_members as groups_members " \
-          "where groups_members.userid = user.id " \
-          "  and groups_members.groupid = " + str(group_id)
+          "     mdl_cohort_members as cohort_members " \
+          "where cohort_members.userid = user.id " \
+          "  and cohort_members.cohortid = " + str(group_id)
     users = User.objects.raw(sql)
     return render(request, 'group_final_grade.html', locals())
